e2fd8e41b734f0a3a9d88f0c170799697ed01041
Konstantinos Sakkas <63365463+ksakkas@users.noreply.github.com>
2021-05-31 11:08:59 +0300

    Create method1.cs

---
+++ Codes/method1.cs
@@ -0,0 +1,11 @@
+// Μια μέθοδος είναι ένα μπλοκ κώδικα που εκτελείται μόνο όταν καλείται.
+
+static void Ditmethod() 
+{
+  Console.WriteLine("hi from Ditmethod!");
+}
+
+static void Main(string[] args)
+{
+  Ditmethod();
+}
